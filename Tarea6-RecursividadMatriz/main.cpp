45085737435247177fbf35528fd94edc0de92b6e
Armando Galván <zareek99@gmail.com>
2015-08-27 07:28:33 -0500

    Initial Commit

---
+++ Tarea6-RecursividadMatriz/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Tarea6-RecursividadMatriz
+//
+//  Created by Zareek Galvan on 8/27/15.
+//  Copyright (c) 2015 Trisquel Labs. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
